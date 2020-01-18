0fc7d194c860991ded2766b372c86487b8072e7e
AMAN SAHARAN <aman@AMANs-MacBook-Pro.local>
2020-01-19 05:21:30 +0530

    Updates

---
+++ precache-manifest.de0bd11264832babfdf8e959d9efa9e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f0d8e5f26ce250db228fcbc87a9fa31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e6e8ff92a4e77ac94a0",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "37e6d00808be085840fb",
+    "url": "/static/css/main.1e5551a0.chunk.css"
+  },
+  {
+    "revision": "8e6e8ff92a4e77ac94a0",
+    "url": "/static/js/2.eb1b30f9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.eb1b30f9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "37e6d00808be085840fb",
+    "url": "/static/js/main.b14626c8.chunk.js"
+  },
+  {
+    "revision": "8963f78c0e1a10b5862f",
+    "url": "/static/js/runtime-main.8b07e775.js"
+  }
+]);
